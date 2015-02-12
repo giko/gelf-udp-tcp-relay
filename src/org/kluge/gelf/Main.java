64c477488155633ddff82e971f70adb2ee090830
nchudakov <n.chudakov@ventra.ru>
2015-02-12 18:12:17 +0300

    fixes, lambdas

--- src/org/kluge/gelf/Main.java
+++ src/org/kluge/gelf/Main.java
@@ -2,28 +2,25 @@ package org.kluge.gelf;
 
 import com.squareup.tape.FileObjectQueue;
 import com.squareup.tape.ObjectQueue;
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.socket.DatagramPacket;
 import io.reactivex.netty.RxNetty;
-import io.reactivex.netty.channel.ConnectionHandler;
-import io.reactivex.netty.channel.ObservableConnection;
-import io.reactivex.netty.pipeline.PipelineConfigurators;
 import io.reactivex.netty.protocol.udp.server.UdpServer;
 import rx.Observable;
 import rx.functions.Func1;
 
-import java.io.*;
-import java.net.InetSocketAddress;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.charset.Charset;
 
 public class Main {
     private static FileObjectQueue<byte[]> messageQueue;
+
     public static void main(String[] args) throws IOException {
         Socket clientSocket = new Socket("gray", 9003);
         clientSocket.setKeepAlive(true);
         OutputStream outToServer = clientSocket.getOutputStream();
-        
+
         messageQueue = new FileObjectQueue<>(new File("fallback"),
                 new FileObjectQueue.Converter<byte[]>() {
                     @Override public byte[] from(byte[] bytes) throws IOException {
@@ -48,7 +45,7 @@ public class Main {
                 }
             }
         });
-        
+
         messageQueue.setListener(new ObjectQueue.Listener<byte[]>() {
             @Override public void onAdd(ObjectQueue<byte[]> queue, byte[] entry) {
                 sender.start();
@@ -58,15 +55,13 @@ public class Main {
 
             }
         });
+        createServer(9003).startAndWait();
     }
 
-    public UdpServer<DatagramPacket, DatagramPacket> createServer(Integer port) {
+    public static UdpServer<DatagramPacket, DatagramPacket> createServer(Integer port) {
         UdpServer<DatagramPacket, DatagramPacket> server = RxNetty
-                .createUdpServer(port, new ConnectionHandler<DatagramPacket, DatagramPacket>() {
-                    @Override
-                    public Observable<Void> handle(
-                            final ObservableConnection<DatagramPacket, DatagramPacket> newConnection) {
-                        return newConnection.getInput().flatMap(new Func1<DatagramPacket, Observable<Void>>() {
+                .createUdpServer(port, newConnection -> newConnection.getInput()
+                        .flatMap(new Func1<DatagramPacket, Observable<Void>>() {
                             @Override
                             public Observable<Void> call(DatagramPacket received) {
                                 synchronized (messageQueue) {
@@ -74,12 +69,8 @@ public class Main {
                                 }
                                 return Observable.empty();
                             }
-                        });
-                    }
-                });
-        
-        
-        System.out.println("UDP hello server started...");
+                        }));
+
         return server;
     }
 }
